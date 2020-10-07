9b2a3942988e38c0a7ad337bf45d47d890929bbb
justintermini <justintermini@gmail.com>
2020-10-07 11:58:45 -0400

    Added comments

--- lines.py
+++ lines.py
@@ -4,25 +4,31 @@ Description: Display requested lines in a file.
 """
 
 def main():
+    # Call the read_file function to create a list from file.
     line_list = read_file()
+    # Get the number of lines in the list and print it .
     line_num = len(line_list)
     print(f"There are {line_num} lines in the file.")
+
+    # Prompt user to choose a line to view. Loop until user chooses '0'.
     while True:
-        num = int(input("Which line number would you like to see?: "))
-        if num == 0:
+        num = int(input("Which line number would you like to see? "
+                        "(Choose '0' to exit): "))
+        if num == 0: # Exit code for program.
             break
-        elif num > int(line_num):
+        elif num > int(line_num): # Produce error if number is too high.
             print(f"[+] Error: there are only {line_num} lines in the file.\n")
-        else:
+        else: # Output line.
             print(line_list[num-1])
 
 def read_file():
-    """Prompt user for filename and read it
-        line-by-line into a list.
-    """
+    """Prompt user for filename and read it line-by-line into a list."""
+    # Prompt user for file name.
     file_name = input("What file would you like to read?: ")
+    # Read file into varaible 'f'.
     f = open(file_name, 'r')
     line_list = []
+    # Split up file line by line into a list, then return list variable.
     while True:
         line = f.readline()
         if line == "":
@@ -30,8 +36,6 @@ def read_file():
         line_list.append(line)
     return line_list
         
-        
-
 if __name__ == "__main__":
     main()
 
